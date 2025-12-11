6bd8ecf8f02219aaeb52af0923a0dd380c72e055
rayhan123-ahmed <rmd09181@gmail.com>
2025-12-11 17:50:32 +0600

    all about dates

---
+++ basic-01/07_dates.js
@@ -0,0 +1,23 @@
+// Dates
+const myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+// This how to set date according to own choice
+const myCreatedDate = new Date("11-12-2025")
+console.log(myCreatedDate.toDateString());
+
+// const myCreatedTime = new Date(2025,11,11,5,28)
+// console.log(myCreatedTime.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(Math.round(Date.now()/1000));
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+
